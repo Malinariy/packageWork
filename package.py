b5247dbc4f1c93d36056b8f9ba4c1c5df5519c01
Malinariy <65512186+Malinariy@users.noreply.github.com>
2020-10-05 18:59:21 +0200

    Update package.py

--- package.py
+++ package.py
@@ -1,12 +1,8 @@
-import os, shutil, pathlib, fnmatch
-
-dst = "G:/Fol/here" #импортирование файлов из src
-src = "G:/резервныве копии с телефона/Huinya" #экспорт файлов
+import os, shutil
+dst = *** #импортирование файлов из src
+src = *** #экспорт файлов
 
 for dirnames in os.listdir(src):
     abs_path = os.path.join(src, dirnames) #путь к папкам, которые находятся в одной папке
-    abs_path_listDir = os.listdir(abs_path)
-    for i in abs_path_listDir:
+    for i in os.listdir(abs_path):
         shutil.move(os.path.join(abs_path, i), os.path.join(dst, i))
-
-
